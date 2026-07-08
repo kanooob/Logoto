78891c4ea45b1e09babab1040c589cec689082b6
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-07-08 12:25:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,13 +58,14 @@ const S4D_WEBSITECREATION_EXPRESS_app = S4D_WEBSITECREATION_EXPRESS();
     }
 
     // check if discord-logs is v2
-    if (!require('./package.json').dependencies['discord-logs'].startsWith("^2.")) {
-      let file = JSON.parse(fs.readFileSync('package.json'))
-      file.dependencies['discord-logs'] = '^2.0.0'
-      fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
-      exec('npm i')
-      throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
-    }
+    
+    //if (!require('./package.json').dependencies['discord-logs'].startsWith("^2.")) {
+      //let file = JSON.parse(fs.readFileSync('package.json'))
+      //file.dependencies['discord-logs'] = '^2.0.0'
+      //fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
+      //exec('npm i')
+      //throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
+    //}
 
     // create a new discord client
     s4d.client = new s4d.Discord.Client({
