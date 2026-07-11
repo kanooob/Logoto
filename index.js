af5ca45d4e6913204fc0cb808149f27d5a41c39b
Galxie_S9 <100624075+kanooob@users.noreply.github.com>
2026-07-11 20:44:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Bot Logoto en ligne !');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur de vérification activé sur le port ${port}`);
+});
 (async()=>{
     // default imports
     const events = require('events');
@@ -51,6 +62,7 @@
     }
 
     // check if discord-logs is v2
+    /*
     if (!require('./package.json').dependencies['discord-logs'].startsWith("^2.")) {
       let file = JSON.parse(fs.readFileSync('package.json'))
       file.dependencies['discord-logs'] = '^2.0.0'
@@ -58,6 +70,7 @@
       exec('npm i')
       throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
     }
+    */
 
     // create a new discord client
     s4d.client = new s4d.Discord.Client({
